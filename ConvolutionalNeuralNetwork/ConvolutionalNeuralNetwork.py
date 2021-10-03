a8d252e3a0ec701adfd6959832de280d4223187e
Rafael Melloni Chacon Arnone <titico0306@gmail.com>
2021-10-03 15:24:46 -0300

    RNN

--- ConvolutionalNeuralNetwork/ConvolutionalNeuralNetwork.py
+++ ConvolutionalNeuralNetwork/ConvolutionalNeuralNetwork.py
@@ -8,13 +8,13 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
                                    shear_range = 0.2,
                                    zoom_range = 0.2,
                                    horizontal_flip = True)
-training_set = train_datagen.flow_from_directory('dataset/training_set',
+training_set = train_datagen.flow_from_directory('training_set',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
-test_set = test_datagen.flow_from_directory('dataset/test_set',
+test_set = test_datagen.flow_from_directory('test_set',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
@@ -32,7 +32,7 @@ cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accura
 
 cnn.fit(x = training_set, validation_data = test_set, epochs = 25)
 
-test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.load_img('single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = cnn.predict(test_image)
@@ -42,4 +42,4 @@ if result[0][0] == 1:
 else:
     prediction = 'cat'
 
-print(prediction)
+print(result)
